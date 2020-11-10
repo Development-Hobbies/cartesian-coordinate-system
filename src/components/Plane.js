82cb3ee890549f5d1f328e01fd31b4bf7577fa09
Vincent Villaluna <villalunavincent@gmail.com>
2020-11-10 10:07:16 +0800

    :sparkles:

---
+++ src/components/Plane.js
@@ -0,0 +1,15 @@
+import "./Plane.css";
+
+import X from "./X";
+import Y from "./Y";
+
+const Plane = () => {
+  return (
+    <div className="plane">
+      <Y />
+      <X />
+    </div>
+  );
+};
+
+export default Plane;
