4dfff66fcbe6c5de1e9ab309acfd0b4822ac66ea
Vincent Villaluna <villalunavincent@gmail.com>
2020-11-11 14:39:33 +0800

    :sparkles:

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
 import Plane from './components/Plane';
+import Point from './components/Point';
 
 function App() {
-  return (
-	  <Plane />
-  );
+	let participants = [
+		{logo: "A", x: 90, y: 50},
+		{logo: "B", x: 50, y: 50},
+		{logo: "C", x: 10, y: 90}
+	];
+	const total = 100;
+	return (
+		<Plane>
+			{
+				participants.map((data) => (
+					<Point x={data.x} y={data.y} total={total}>
+						<h1>{ data.logo }</h1>
+					</Point>
+				))
+			}
+		</Plane>
+	);
 }
 
 export default App;
